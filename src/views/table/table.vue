d76400b60360938796ac7b009305c4e07b988d30
Xu <cwxuf@isoftstone.com>
2019-09-02 14:02:28 +0800

    update

---
+++ src/views/table/table.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    {{name}}
+  </div>
+</template>
+<script>
+export default {
+  name: 'table',
+  data() {
+    return {
+      title: 'table',
+    };
+  },
+};
+</script>
+<style lang="sass" scoped>
+
+</style>
